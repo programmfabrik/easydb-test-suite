9daae68f2f2e75711e478e10dfacbc6c1c4c034d
Peter Bangert <peter@programmfabrik.de>
2018-01-31 10:45:41 +0100

    added description to README

---
+++ test_suite_head/12_CIModel/Information.js
@@ -0,0 +1,18 @@
+
+import { Selector } from 'testcafe';
+
+
+
+
+fixture `Information`
+    .page `http://5.peter.pf-berlin.de/?login=root&password=admin`;
+
+
+
+
+test('Information', async t => {
+    await t
+        .wait(5000)
+        .click( Selector('[ez5-loca-key="root.menu.burger"] .cui-button-left'))
+        .click( Selector('[data-qa="root-menu-app-root-menu-about-label"] .cui-button-center'));
+});
