1cc549132bc9fe2e999a9ba4efe3b47b6287e2bd
Peter Bangert <peter@programmfabrik.de>
2018-01-31 11:12:25 +0100

    changes to information test

--- test_suite_head/12_CIModel/Information.js
+++ test_suite_head/12_CIModel/Information.js
@@ -13,6 +13,6 @@ fixture `Information`
 test('Information', async t => {
     await t
         .wait(5000)
-        .click( Selector('[ez5-loca-key="root.menu.burger"]').child('.cui-button-left'))
-        .click( Selector('[data-qa="root-menu-app-root-menu-about-label"]').child('.cui-button-center'));
+        .click( '[ez5-loca-key="root.menu.burger"] .cui-button-left')
+        .click('[data-qa="root-menu-app-root-menu-about-label"] .cui-button-center');
 });
