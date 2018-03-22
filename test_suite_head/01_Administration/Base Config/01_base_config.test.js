bfeac30e003f210b150c586b2c07b0d26e2eedff
Peter Bangert <peter@programmfabrik.de>
2018-03-22 15:51:35 +0100

    added server reboot test

--- test_suite_head/01_Administration/Base Config/01_base_config.test.js
+++ test_suite_head/01_Administration/Base Config/01_base_config.test.js
@@ -1,31 +1,9 @@
 "@fixture 01_Base Config";
 "@page http://10.122.4.25/?login=root&password=admin";
 
-"@test"["02_Allgemein"] = {
-    "1.Wait 50 milliseconds": function() {
-        act.wait(5000);
-    },
-    "2.Click div": function() {
-        act.click('[ez5-loca-key="root.menu.submenu.administration"] .fa-cogs');
-    },
-    '3.Click span "BasisKonfiguration"': function() {
-        var actionTarget = function() {
-            return $(":containsExcludeChildren(BasisKonfiguration)").eq(1);
-        };
-        act.click(actionTarget);
-    },
-    '4.Click span "Allgemein"': function() {
-        act.click('[data-qa="base-config-manager-general-tab-button"] .cui-button-center');
-    },
-    "5.Type in input": function() {
-        act.type('[data-qa="base-config-manager-value-multiinput"] [cui-data-field-name="de-DE"] input', "TestCafe");
-    },
-    "6.Api Calls": function() {
-        act.click('[data-qa="base-config-manager-enabled-select"] .cui-button-visual');
-    },
-    
-    '8.Click span "Speichern"': function() {
-        act.click(".cui-button[data-qa='base-config-manager-save-button'] .cui-button-center");
+"@test"["00_Wait_for_server_initialization"] = {
+    "1.Wait 8 seconds": function() {
+        act.wait(8000);
     }
 };
 
@@ -67,6 +45,36 @@
         act.click('[data-qa="base-config-manager-server-config-tab-button"] .cui-button-center');
     }
 };
+
+
+"@test"["02_Allgemein"] = {
+    "1.Wait 50 milliseconds": function() {
+        act.wait(5000);
+    },
+    "2.Click div": function() {
+        act.click('[ez5-loca-key="root.menu.submenu.administration"] .fa-cogs');
+    },
+    '3.Click span "BasisKonfiguration"': function() {
+        var actionTarget = function() {
+            return $(":containsExcludeChildren(BasisKonfiguration)").eq(1);
+        };
+        act.click(actionTarget);
+    },
+    '4.Click span "Allgemein"': function() {
+        act.click('[data-qa="base-config-manager-general-tab-button"] .cui-button-center');
+    },
+    "5.Type in input": function() {
+        act.type('[data-qa="base-config-manager-value-multiinput"] [cui-data-field-name="de-DE"] input', "TestCafe");
+    },
+    "6.Api Calls": function() {
+        act.click('[data-qa="base-config-manager-enabled-select"] .cui-button-visual');
+    },
+    
+    '8.Click span "Speichern"': function() {
+        act.click(".cui-button[data-qa='base-config-manager-save-button'] .cui-button-center");
+    }
+};
+
 
 
 
