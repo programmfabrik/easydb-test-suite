d945611f79eea4ec9bff06e010f4ab62654cc3c8
Peter Bangert <peter@programmfabrik.de>
2018-03-23 14:20:52 +0100

    adding screenshots to 01 baseconfig test

--- test_suite_head/01_Administration/Base Config/01_base_config.test.js
+++ test_suite_head/01_Administration/Base Config/01_base_config.test.js
@@ -20,26 +20,50 @@
         };
         act.click(actionTarget);
     },
+    "1.Take a screenshot": function() {
+        act.screenshot();
+    },
     '3.Click span "Allgemein"': function() {
         act.click('[data-qa="base-config-manager-general-tab-button"] .cui-button-center');
+    },
+    "1.Take a screenshot": function() {
+        act.screenshot();
     },
     '4.Click span "Hochladen"': function() {
         act.click('[data-qa="base-config-manager-upload-tab-button"] .cui-button-center');
     },
+    "1.Take a screenshot": function() {
+        act.screenshot();
+    },
     '5.Click span "Anmelden"': function() {
         act.click('[data-qa="base-config-manager-login-tab-button"] .cui-button-center');
+    },
+    "1.Take a screenshot": function() {
+        act.screenshot();
     },
     '6.Click span "Design"': function() {
         act.click('[data-qa="base-config-manager-css-tab-button"] .cui-button-center');
     },
+    "1.Take a screenshot": function() {
+        act.screenshot();
+    },
     '7.Click span "Export und OAI/PMH"': function() {
         act.click('[data-qa="base-config-manager-export-tab-button"] .cui-button-center');
+    },
+    "1.Take a screenshot": function() {
+        act.screenshot();
     },
     '8.Click span "Cloud-Dienstleister"': function() {
         act.click('[data-qa="base-config-manager-cloud_services-tab-button"] .cui-button-center');
     },
+    "1.Take a screenshot": function() {
+        act.screenshot();
+    },
     '9.Click span "CMS"': function() {
         act.click('[data-qa="base-config-manager-cms-tab-button"] .cui-button-center');
+    },
+    "1.Take a screenshot": function() {
+        act.screenshot();
     },
     '10.Click span "Server-Config"': function() {
         act.click('[data-qa="base-config-manager-server-config-tab-button"] .cui-button-center');
