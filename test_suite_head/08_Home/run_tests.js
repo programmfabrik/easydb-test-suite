892b71e7daf87fc8fd4e6e219117a98f62a6c5bf
Peter Bangert <peter@programmfabrik.de>
2018-03-23 16:59:18 +0100

    adding run_tests.js to see if it works with screenshots

--- test_suite_head/08_Home/run_tests.js
+++ test_suite_head/08_Home/run_tests.js
@@ -4,7 +4,7 @@ createTestCafe('localhost', 1337, 1338)
 	.then(testcafe => {
 		const runner = testcafe.createRunner();
 		return runner
-		.src(['screenshots_cnn.test.js'])
+		.src(['test_suite_head/08_Home/screenshots_cnn.test.js'])
 		.browsers(['firefox'])
 		.screenshots('./screenshots/', true)
 		.run();
