1a2a23eed58aab638c90dfd9be1e395f9861ecbd
Peter Bangert <peter@programmfabrik.de>
2018-04-17 15:55:50 +0100

    added the show thumbnails

--- test_suite_head/01_Administration/Data Model/01_administration_data_model.test.js
+++ test_suite_head/01_Administration/Data Model/01_administration_data_model.test.js
@@ -36,7 +36,80 @@
     }
 };
  
+"@test"["01_Viewable_in_Alle_Felder"] = {
+    "1.Wait 5000 milliseconds": function() {
+        act.wait(5e3);
+    },"1.Take a screenshot": function() {act.screenshot();},
+    "2.Click <i>": function() {
+        act.click('[row="0"] .fa-caret-right');
+    },"1.Take a screenshot": function() {act.screenshot();},
+    '3.Click span "allefelder__all_fie..."': function() {  
+        var actionTarget = function() {
+            return $('.datamodel-list-view-mask [row="9"] span');
+        };
+        act.click(actionTarget);
+    },"1.Take a screenshot": function() {act.screenshot();},
+    '4.Click span "Definition"': function() {
+        act.click(":containsExcludeChildren(Definition)");
+    },"1.Take a screenshot": function() {act.screenshot();},
+    "5.Click div": function() {
+        act.click('[row="11"] [col="13"] .cui-button-visual');
+    },"1.Take a screenshot": function() {act.screenshot();},
+    '6.Click span "Anzeigen"': function() {
+        var actionTarget = function() {
+            return $(":containsExcludeChildren(Anzeigen)").eq(3);
+        };
+        act.click(actionTarget);
+    },"1.Take a screenshot": function() {act.screenshot();},
+    "7.Click div": function() {
+        act.click(".cui-layer-backdrop");
+    },"1.Take a screenshot": function() {act.screenshot();},
+    '8.Click span "Speichern"': function() {
+        var actionTarget = function() {
+            return $(":containsExcludeChildren(Speichern)").eq(1);
+        };
+        act.click(actionTarget);
+    },"1.Take a screenshot": function() {act.screenshot();},
+    
+};
 
+
+
+"@test"["02_Viewable_in_Dateien"] = {
+    "1.Wait 5000 milliseconds": function() {
+        act.wait(5e3);
+    },"1.Take a screenshot": function() {act.screenshot();},
+    "15.Click <i>": function() {
+        act.click('[row="1"] .fa-caret-right');
+    },"1.Take a screenshot": function() {act.screenshot();},
+    '16.Click span "dateien__all_fields"': function() {
+        var actionTarget = function() {
+            return $('.datamodel-list-view-mask [row="10"] span');
+        };
+        act.click(actionTarget);
+    },"1.Take a screenshot": function() {act.screenshot();},
+    '17.Click span "Definition"': function() {
+        act.click(":containsExcludeChildren(Definition)");
+    },"1.Take a screenshot": function() {act.screenshot();},
+    "18.Click div": function() {
+        act.click('.cui-list-view-grid-inner-bottom [row="3"] [col="13"] .cui-button-visual');
+    },"1.Take a screenshot": function() {act.screenshot();},
+    '19.Click span "Anzeigen"': function() {
+        var actionTarget = function() {
+            return $(":containsExcludeChildren(Anzeigen)").eq(1);
+        };
+        act.click(actionTarget);
+    },"1.Take a screenshot": function() {act.screenshot();},
+    "20.Click div": function() {
+        act.click(".cui-layer-backdrop");
+    },"1.Take a screenshot": function() {act.screenshot();},
+    '21.Click span "Speichern"': function() {
+        var actionTarget = function() {
+            return $(":containsExcludeChildren(Speichern)").eq(1);
+        };
+        act.click(actionTarget);
+    }
+};
 
 "@test"["04_Commit"] = {
     "1.Wait 5000 milliseconds": function() {
